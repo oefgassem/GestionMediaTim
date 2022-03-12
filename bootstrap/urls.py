cf0457c160ac83b57cd61923440e6a2a2089164d
Omar El Farouk GASSEM <oefgassem@gmail.com>
2022-03-12 23:30:01 +0100

    modification login

--- bootstrap/urls.py
+++ bootstrap/urls.py
@@ -18,9 +18,12 @@ from django.urls import path
 from bootstrap import settings
 from django.conf.urls.static import static
 from app import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('<filename>.html', views.html),
     path('', views.index),
     ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
